a774368aa5114b02d8617d5e943ead95df65a238
pakhomovas <art.pa99@mail.ru>
2025-03-23 14:33:21 +0300

    написали котекстный менеджер

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,3 +1,4 @@
+
 from hamcrest import (
     assert_that,
     has_properties,
@@ -8,56 +9,62 @@ from hamcrest import (
     contains_inanyorder,
 )
 
+from checkers.http_checkers import check_status_code_http
+
 
 def test_get_v1_account_auth(auth_account_helper):
-    response = auth_account_helper.dm_account_api.account_api.get_v1_account(
-        validate_response=True
-    )
-    assert_that(
-        response,
-        all_of(
-            has_property("resource", has_property("login", starts_with("Good"))),
-            has_property(
-                "resource",
-                has_properties({"roles": contains_inanyorder("Guest", "Player")}),
-            ),
-            has_property(
-                "resource",
+
+        response = auth_account_helper.dm_account_api.account_api.get_v1_account(
+            validate_response=True
+        )
+        assert_that(
+            response,
+            all_of(
+                has_property("resource", has_property("login", starts_with("Good"))),
                 has_property(
-                    "settings",
+                    "resource",
+                    has_properties({"roles": contains_inanyorder("Guest", "Player")}),
+                ),
+                has_property(
+                    "resource",
+                    has_property(
+                        "settings",
+                        has_properties(
+                            {
+                                "colorSchema": equal_to("Modern"),
+                                "paging": has_properties(
+                                    {
+                                        "postsPerPage": equal_to(10),
+                                        "commentsPerPage": equal_to(10),
+                                        "topicsPerPage": equal_to(10),
+                                        "messagesPerPage": equal_to(10),
+                                        "entitiesPerPage": equal_to(10),
+                                    }
+                                ),
+                            }
+                        ),
+                    ),
+                ),
+                has_property(
+                    "resource",
                     has_properties(
                         {
-                            "colorSchema": equal_to("Modern"),
-                            "paging": has_properties(
+                            "rating": has_properties(
                                 {
-                                    "postsPerPage": equal_to(10),
-                                    "commentsPerPage": equal_to(10),
-                                    "topicsPerPage": equal_to(10),
-                                    "messagesPerPage": equal_to(10),
-                                    "entitiesPerPage": equal_to(10),
+                                    "enabled": equal_to(True),
+                                    "quality": equal_to(0),
+                                    "quantity": equal_to(0),
                                 }
-                            ),
+                            )
                         }
                     ),
                 ),
             ),
-            has_property(
-                "resource",
-                has_properties(
-                    {
-                        "rating": has_properties(
-                            {
-                                "enabled": equal_to(True),
-                                "quality": equal_to(0),
-                                "quantity": equal_to(0),
-                            }
-                        )
-                    }
-                ),
-            ),
-        ),
-    )
+        )
 
 
 def test_get_v1_account_no_auth(account_helper):
-    account_helper.dm_account_api.account_api.get_v1_account(validate_response=False)
+    with check_status_code_http(401, "User must be authenticated"):
+        account_helper.dm_account_api.account_api.get_v1_account(
+            validate_response=False
+        )
