54eda1eb8a0fbf98d53670504d08853dd6c76586
pakhomovas <art.pa99@mail.ru>
2025-03-28 23:15:10 +0300

    coverage

--- restclient/client.py
+++ restclient/client.py
@@ -2,6 +2,10 @@ from requests import session, JSONDecodeError
 import structlog
 import uuid
 import curlify
+from requests.auth import HTTPBasicAuth
+from swagger_coverage_py.listener import CoverageListener
+from swagger_coverage_py.request_schema_handler import RequestSchemaHandler
+from swagger_coverage_py.uri import URI
 
 from restclient.configuration import Configuration
 from restclient.utilites import allure_attach
@@ -51,6 +55,12 @@ class RestClient:
                 )
         rest_response = self.session.request(method=method, url=full_url, **kwargs)
         curl = curlify.to_curl(rest_response.request)
+
+        uri = URI(host=self.host, base_path="", unformatted_path=path, uri_params=kwargs.get("params"))
+        RequestSchemaHandler(
+            uri, method.lower(), rest_response, kwargs
+        ).write_schema()
+
         print(curl)
         log.msg(event='Response',
                 status_code=rest_response.status_code,
